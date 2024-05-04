4221d33e6998611c8dc22fcc355d85945b2f5f58
Sanjaee <afrizaahmad18@gmail.com>
2024-05-04 08:57:11 +0700

    page layout

--- src/Pages/VerivyPage.jsx
+++ src/Pages/VerivyPage.jsx
@@ -45,7 +45,7 @@ const Verify = () => {
   return (
     <>
       <Navbar>
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-3 ">
           <Link
             className="px-2 py-2 text-[14px] rounded-[4px] text-[#000000] font-bold "
             to={"/"}
@@ -60,7 +60,7 @@ const Verify = () => {
           </Link>
         </div>
       </Navbar>
-      <div className="flex items-center justify-center mt-20 mx-auto">
+      <div className="flex items-center justify-center mt-20 mx-4 ">
         <div className="flex flex-col">
           <h1 className="text-[24px] text-[#2F2F2F] font-bold">
             Verify Your Email to Get Started
@@ -69,7 +69,7 @@ const Verify = () => {
             <div className="max-w-[510px] w-full py-4 pl-4 pr-20 bg-[#FFFFFF] mt-10">
               <p className="text-[#79889D]">
                 A confirmation link has been sent to your email address{" "}
-                <span className="font-bold">{email}</span> Click the link to
+                <span className="font-bold">{email}</span> Enter the OTP code to
                 verify your account and unlock full access.
               </p>
               <div className="flex flex-col mt-6">
